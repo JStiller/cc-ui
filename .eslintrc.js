ec023c524d664171ce9654f42e33b1ec859d8856
José Stiller <JStiller@users.noreply.github.com>
2021-09-14 20:05:15 +0200

    added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:@stencil/recommended",
+        "prettier",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+    },
+    plugins: ["@typescript-eslint"],
+};
